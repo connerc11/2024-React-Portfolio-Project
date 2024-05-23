e04aceb43c2a0f57738a782dbeb567b6e0170e8c
connerc11 <cccochrane1@gmail.com>
2024-05-23 16:37:36 -0400

    Updated the about me column with some experience and skills

--- React 2024 portfolio/src/App.jsx
+++ React 2024 portfolio/src/App.jsx
@@ -2,18 +2,21 @@
 //import reactLogo from './assets/react.svg'
 //  import viteLogo from '/vite.svg'
 import styles from "./App.module.css";
+import { Experience } from "./components/Experience/Experience";
+import { About } from "./components/About/About";
 import { Navbar } from './components/Navbar/Navbar'
+import { Hero } from "./components/Hero/Hero";
 //import './vars.css'
 
 function App() {
   return (
     <div className={styles.App}>
       <Navbar />
-       <Hero />
-      { /* <About />
-      <Experience />
-      <Projects />
-      <Contact /> */}
+      <Hero />
+       <About />
+       <Experience />
+      {/* <Projects />
+      <Contact />   */}
     </div>
   );
 }
