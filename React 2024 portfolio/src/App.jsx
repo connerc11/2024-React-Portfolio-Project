70052626cf6c46e2ca45fefa4effef3eff7ebc22
connerc11 <cccochrane1@gmail.com>
2024-05-23 17:34:18 -0400

    added in some contact for the contact me portion

--- React 2024 portfolio/src/App.jsx
+++ React 2024 portfolio/src/App.jsx
@@ -2,9 +2,11 @@
 //import reactLogo from './assets/react.svg'
 //  import viteLogo from '/vite.svg'
 import styles from "./App.module.css";
+import { Contact } from "./components/Contact/Contact";
 import { Projects } from "./components/Projects/Projects";
 import { Experience } from "./components/Experience/Experience";
 import { About } from "./components/About/About";
+// import {Certifications} from "./components/Certifications/Certifications"
 import { Navbar } from './components/Navbar/Navbar'
 import { Hero } from "./components/Hero/Hero";
 //import './vars.css'
@@ -17,7 +19,8 @@ function App() {
        <About />
        <Experience />
       < Projects /> 
-      {/* <Contact />    */}
+      {/* <Certifications/> */}
+      <Contact/>
     </div>
   );
 }
