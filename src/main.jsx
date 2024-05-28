74c44d5d7d3c20c83ab88f3df6ca077108966c42
connerc11 <cccochrane1@gmail.com>
2024-05-28 15:12:57 -0400

    push

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.js";
+import App from "./App.jsx";
 import "./index.css";
 // import "@fontsource/outfit";
 // import "@fontsource/roboto";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <HashRouter>
     <App />
+    </HashRouter>
   </React.StrictMode>
 );
